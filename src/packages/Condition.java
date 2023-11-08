d1b8a035607885c4a8ed723595c7cf2af395a9b1
devkoyote <alencar.gestao@gmail.com>
2023-11-08 10:43:32 -0300

    studies conditions

---
+++ src/packages/Condition.java
@@ -0,0 +1,27 @@
+package packages;
+
+import java.util.Scanner;
+
+public class Condition {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if(hora <= 12) {
+            System.out.println("Bom dia!");
+        }
+        if(hora > 12 || hora <= 18) {
+            System.out.println("Boa Tarde!");
+        }
+        if(hora > 18 || hora <= 24) {
+            System.out.println("Boa noite!");
+        }
+
+        sc.close();
+        
+    }
+}
