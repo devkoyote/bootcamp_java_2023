1b7b5d20a03ab39798ca33769382141642102388
devkoyote <alencar.gestao@gmail.com>
2023-11-15 12:12:43 -0300

    project 4 employee finish

---
+++ src/packages/project4/Employee.java
@@ -0,0 +1,24 @@
+package packages.project4;
+
+public class Employee {
+    
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    // functions
+    public double netSalary(){
+        return this.grossSalary -= this.tax;
+    }
+
+    public void increaseSalary(double percentage){
+        double rs = (this.grossSalary * percentage) / 100;
+        this.grossSalary += rs;
+    }
+
+    // sobreposição de dados
+    public String toString() {
+        return name + ", " + "R$"+ String.format("%.2f", grossSalary) + "; "; 
+    }
+
+}
---
+++ src/packages/project4/Program.java
@@ -0,0 +1,36 @@
+package packages.project4;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Employee emp = new Employee();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o nome do funcionário: ");
+        emp.name = sc.nextLine();
+        System.out.println();
+        System.out.println("Digite o salario bruto: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.println();
+        System.out.println("Digite o valor do imposto pago: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Passe porcentagem para a correção salarial ser recalculada: ");
+        double perc = sc.nextDouble();
+        // calcula porcentagem e soma ao salario
+        emp.increaseSalary(perc);
+        // calcula o imposto a ser cobrado
+        emp.netSalary();
+        
+        System.out.println();
+        System.out.println("Dados atualiza dos:" + emp);
+        
+
+
+        sc.close();
+
+    }
+}
