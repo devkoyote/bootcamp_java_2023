c3a63385542935342857a964b640d85c84f486b7
devkoyote <alencar.gestao@gmail.com>
2023-11-18 16:33:32 -0300

    project 6 membros estáticos

---
+++ src/packages/project6/Program.java
@@ -0,0 +1,42 @@
+package packages.project6;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Program
+ */
+public class Program {
+
+    public static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com o raio: ");
+        double radius = sc.nextDouble();
+
+        double c = circunference(radius);
+
+        double v = volume(radius);
+
+        System.out.printf("Circuferência: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI valor: %.2f%n", PI);
+
+        sc.close();
+    }
+
+    // functions
+    public static double circunference(double radius) {
+        return 2.0 * PI * radius;
+    }
+
+    public static double volume(double radius) {
+        return 4.0 * PI * radius * radius * radius / 3;
+    }
+
+
+
+}
